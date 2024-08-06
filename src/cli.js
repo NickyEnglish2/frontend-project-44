32a16236518a03415fbf512ebefc7573d7e9750b
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2024-08-06 20:15:46 +0800

    Modified cli.js file for index.js purposes

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const askingQuestion = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
-};
-
 const greetingUser = () => {
   console.log('Welcome to the Brain Games!');
-  askingQuestion();
+  const userName = readlineSync.question('May I have your name? ');
+  return userName;
 };
 
 export default greetingUser;
