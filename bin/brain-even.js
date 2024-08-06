5f79fb9463b7bccda112b6309d58b179f407b46c
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2024-08-06 20:16:57 +0800

    Adapted brain-even game for new game logic in index.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-import greetingUser from '../src/cli.js';
-import playGameBrainEven from '../src/even.js';
+import playGame from '../src/index.js';
+import generateQuestionsForEven from '../src/even.js';
 
-greetingUser();
-
-playGameBrainEven();
+playGame('Answer "yes" if the number is even, otherwise answer "no".', generateQuestionsForEven);
--- src/even.js
+++ src/even.js
@@ -1,32 +1,13 @@
-import readlineSync from 'readline-sync';
+import randomNumber from './randomnumber.js';
 
-const randomNumber = () => Math.floor(Math.random() * 100);
 const isEven = (num) => num % 2 === 0;
 
-const playGameBrainEven = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const generateQuestionsForEven = () => {
+  const num1 = randomNumber();
 
-  for (let i = 1; i <= 3; i += 1) {
-    const numberForQuestion = randomNumber();
+  const answer = isEven(num1) ? 'yes' : 'no';
 
-    console.log(`Question: ${numberForQuestion}`);
-    const userAnswer = readlineSync.question('Your Answer: ');
-
-    const correctAnswer = isEven(numberForQuestion) ? 'yes' : 'no';
-
-    if (userAnswer !== 'yes' && userAnswer !== 'no') {
-      console.log(`'${userAnswer}' is wrong ;(. Correct answer was '${correctAnswer}'`);
-      return;
-    }
-
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong ;(. Correct answer was '${correctAnswer}'`);
-      return;
-    }
-  }
-  console.log('Congratulations!');
+  return [num1, answer];
 };
 
-export default playGameBrainEven;
+export default generateQuestionsForEven;
